c664822fcc8104bbb7b12274f606a4753b9d5dee
JORGE_CRUZ <jorgeluiscruzcusihuaman@hotmail.com>
2024-06-26 23:42:20 -0500

    arreglamos la configuracion de la base de datos del sistema

--- app/config.php
+++ app/config.php
@@ -14,24 +14,19 @@ $servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
 $dia_actual = date("d"); //dia
 $mes_actual = date("m"); //mes
 
-
-if ($dia_actual >= 01 && $mes_actual == 01) {
-
-    
-}else{
-    try {
-        $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        //echo "La conexión a la base de datos fue con exito";
-    } catch (PDOException $e) {
-        //print_r($e);
-        echo "Error al conectar a la base de datos";
-    }
-    
-    $URL = "http://10.200.0.21/sistemadedevoluciondedinerounfv";
-    
-    date_default_timezone_set("America/Lima");
-    $fechaHora = date('Y-m-d H:i:s');
+try {
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    //echo "La conexión a la base de datos fue con exito";
+} catch (PDOException $e) {
+    //print_r($e);
+    echo "Error al conectar a la base de datos";
 }
+
+$URL = "http://10.200.0.21/sistemadedevoluciondedinerounfv";
+
+date_default_timezone_set("America/Lima");
+$fechaHora = date('Y-m-d H:i:s');
+
 
 
 
